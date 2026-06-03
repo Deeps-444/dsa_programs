9d2bd9aa4ec5bf261e6d2e20ec3d45408a79a8f4
Deepti <deeptipal444@gmail.com>
2026-06-03 09:24:12 +0530

    binary search and recursion

--- java_dsa/Rec_09.java
+++ java_dsa/Rec_09.java
@@ -1,5 +1,28 @@
 package java_dsa;
 
 public class Rec_09 {
+    // move all x to the end of string
+    public static void rec(String s, int idx){
+        if(idx == s.length()){
+            return;
+        }
+
+        if(s.charAt(idx) != 'x'){
+            System.out.print(s.charAt(idx));
+        }
+        rec(s, idx+1);
+        if(s.charAt(idx) == 'x'){
+            System.out.print(s.charAt(idx));
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "axbcxxe";
+        System.out.println("s before: " + s);
+        System.out.println("s after: ");
+        rec(s, 0);
+
+        return;
+    }
 
 }
---
+++ java_dsa/Rec_10.java
@@ -0,0 +1,10 @@
+package java_dsa;
+
+public class Rec_10 {
+    //removing duplicates
+    public static void removeDupe()
+
+    public static void main(String[] args) {
+        return;
+    }
+}
